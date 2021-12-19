48634df8a3707f4d2b7bd2bb358e49ba745a6e3f
Anony Mous <zhangdanhua3110@gmail.com>
2021-12-19 12:08:31 -0600

    upload

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    public void OnClickButton()
+    {
+        Debug.Log("Button is clicked");
+    }
+}
